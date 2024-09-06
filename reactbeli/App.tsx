cbe36a0ca0e44735dc00d1475f752659e437a148
sorrisin <159536463+belisariosc@users.noreply.github.com>
2024-09-06 11:49:00 -0300

    a
    a

--- reactbeli/App.tsx
+++ reactbeli/App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View,Text,StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+export default function app (){
+  return(
+  <View>
+    <View>
+
+     </View>
+     <View>
+
+     </View>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+)}
