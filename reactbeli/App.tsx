a2ee441e8f326bcb2b3254f9426b8212bad099ab
joaopedrordm77 <159532392+joaopedrordm77@users.noreply.github.com>
2024-09-20 08:38:42 -0300

    aaaaaa
    aaaaaa

--- reactbeli/App.tsx
+++ reactbeli/App.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import {View,Text,StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 
 export default function app (){
   return(
-  <View>
-    <View>
-
-     </View>
-     <View>
-
-     </View>
+    <View style={styles.container}>
+      <View style={styles.box1}>
+        <Text>Box 1</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text>Box 2</Text>
+      </View>
     </View>
-)}
+  )
+}
+
+const styles = StyleSheet. create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+})
